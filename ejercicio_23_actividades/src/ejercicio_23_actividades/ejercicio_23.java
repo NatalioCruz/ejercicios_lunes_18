cb5e08ba2ab6a31a97af461ae0d357e9db6d4138
Natalio Cruz <nataliocg16@gmail.com>
2021-01-18 18:43:54 +0100

    primer commit

---
+++ ejercicio_23_actividades/src/ejercicio_23_actividades/ejercicio_23.java
@@ -0,0 +1,17 @@
+package ejercicio_23_actividades;
+
+import java.util.Arrays;
+
+public class ejercicio_23 {
+	
+	 public static void main(String[] args)
+	  {
+	        int arr1[] = {5, 7, 6, 2, 5, 7, 7, 10}; 
+	        int arr2[] = {1, 3, 9, 10}; 
+	        if (Arrays.equals(arr1, arr2)) 
+	            System.out.println("IGUALES"); 
+	        else
+	            System.out.println("NO IGUALES"); 
+	  }
+
+}
