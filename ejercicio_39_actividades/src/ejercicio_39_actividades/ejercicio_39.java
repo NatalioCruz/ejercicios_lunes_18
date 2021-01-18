efd986af89c56169cd1e8487b7123015e4462048
Natalio Cruz <nataliocg16@gmail.com>
2021-01-18 18:48:49 +0100

    primer commit

---
+++ ejercicio_39_actividades/src/ejercicio_39_actividades/ejercicio_39.java
@@ -0,0 +1,27 @@
+package ejercicio_39_actividades;
+
+import java.util.Scanner;
+
+public class ejercicio_39 {
+	
+	public static void main(String[] args) {
+		 
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduce el número de elementos a mostrar de la serie: ");
+        int limite = sc.nextInt();
+        sc.close();
+
+        for(int i = 0; i<limite; i++){
+             System.out.print(funcionFibonacci(i) + ", ");
+        }
+   }
+
+   private static int funcionFibonacci(int num){
+        if(num == 0 || num==1)
+             return num;
+        else
+             return funcionFibonacci(num-1) + funcionFibonacci(num-2);
+   }
+
+
+}
